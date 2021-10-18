30a978bdda6c7c63d9ef4382bdf7f29577dae116
Gaute Lindkvist <lindkvis@gmail.com>
2021-10-18 12:30:20 +0200

    Remove FIFO fields

--- ProjectDataModel/IoCore/cafFieldIoCapabilitySpecializations.h
+++ ProjectDataModel/IoCore/cafFieldIoCapabilitySpecializations.h
@@ -3,7 +3,6 @@
 #include "cafChildArrayField.h"
 #include "cafChildField.h"
 #include "cafFieldIoCapability.h"
-#include "cafFifoField.h"
 
 #include <nlohmann/json.hpp>
 
@@ -71,58 +70,6 @@ public:
 
 private:
     FieldType* m_field;
-};
-
-template <typename DataType>
-class FieldIoCap<FifoBlockingField<DataType>> : public FieldIoCapability
-{
-    using FieldType = FifoBlockingField<DataType>;
-
-public:
-    FieldIoCap( FieldType* field, bool giveOwnership )
-        : FieldIoCapability( field, giveOwnership )
-        , m_readLimit( 10u )
-    {
-        m_field = field;
-    }
-
-public:
-    // Json Serializing
-    void readFromJson( const nlohmann::json& jsonValue, ObjectFactory* objectFactory, bool copyDataValues ) override;
-    void writeToJson( nlohmann::json& jsonValue, bool copyDataValues ) const override;
-
-    void   setMaximumPackagesForIoOutput( size_t maximumPackageCount ) { m_readLimit = maximumPackageCount; }
-    size_t maximumPackagesForIoOutput() const { return m_readLimit; }
-
-private:
-    FieldType* m_field;
-    size_t     m_readLimit;
-};
-
-template <typename DataType>
-class FieldIoCap<FifoBoundedField<DataType>> : public FieldIoCapability
-{
-    using FieldType = FifoBoundedField<DataType>;
-
-public:
-    FieldIoCap( FieldType* field, bool giveOwnership )
-        : FieldIoCapability( field, giveOwnership )
-        , m_readLimit( 10u )
-    {
-        m_field = field;
-    }
-
-public:
-    // Json Serializing
-    void readFromJson( const nlohmann::json& jsonValue, ObjectFactory* objectFactory, bool copyDataValues ) override;
-    void writeToJson( nlohmann::json& jsonValue, bool copyDataValues ) const override;
-
-    void   setMaximumPackagesForIoOutput( size_t maximumPackageCount ) { m_readLimit = maximumPackageCount; }
-    size_t maximumPackagesForIoOutput() const { return m_readLimit; }
-
-private:
-    FieldType* m_field;
-    size_t     m_readLimit;
 };
 
 template <typename FieldType>
