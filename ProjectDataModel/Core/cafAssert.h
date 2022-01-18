6eca4789439a5d3c516a43bbd9fead00ce79aee6
Gaute Lindkvist <lindkvis@gmail.com>
2022-01-18 08:47:28 +0100

    Improve CAFFA_ASSERT

--- ProjectDataModel/Core/cafAssert.h
+++ ProjectDataModel/Core/cafAssert.h
@@ -3,15 +3,19 @@
 
 #include "cafLogger.h"
 
+#include <assert.h>
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 
-#define CAFFA_ASSERT( expr )                                                                           \
-    do                                                                                                 \
-    {                                                                                                  \
-        if ( !( expr ) )                                                                               \
-        {                                                                                              \
-            CAFFA_CRITICAL( __FILE__ << ":" << __LINE__ << ": CAFFA_ASSERT(" << #expr << ") failed" ); \
-            std::abort();                                                                              \
-        }                                                                                              \
+#define CAFFA_ASSERT( expr )                                                                \
+    do                                                                                      \
+    {                                                                                       \
+        if ( !( expr ) )                                                                    \
+        {                                                                                   \
+            std::stringstream str;                                                          \
+            str << __FILE__ << ":" << __LINE__ << ": CAFFA_ASSERT(" << #expr << ") failed"; \
+            CAFFA_CRITICAL( str.str() );                                                    \
+            std::abort();                                                                   \
+        }                                                                                   \
     } while ( false )
