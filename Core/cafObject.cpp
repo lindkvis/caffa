7b9e500503367f7159902f394150ed2a0a0f19cb
Gaute Lindkvist <lindkvis@gmail.com>
2022-05-16 08:14:26 +0200

    Be more explicit about seed in cafObject

--- Core/cafObject.cpp
+++ Core/cafObject.cpp
@@ -9,6 +9,8 @@ using namespace caffa;
 
 CAFFA_ABSTRACT_SOURCE_INIT( Object, "Object" )
 
+using namespace std::chrono;
+
 //--------------------------------------------------------------------------------------------------
 ///
 //--------------------------------------------------------------------------------------------------
@@ -16,8 +18,13 @@ Object::Object()
     : ObjectHandle()
     , ObjectIoCapability( this, false )
 {
-    auto                         seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::mt19937                 generator( seed );
+    auto     now                     = system_clock::now();
+    auto     nanoseconds_since_epoch = now.time_since_epoch();
+    auto     seconds_since_epoch     = duration_cast<seconds>( nanoseconds_since_epoch );
+    unsigned nanoseconds_since_last_second =
+        static_cast<unsigned>( ( nanoseconds_since_epoch - duration_cast<nanoseconds>( seconds_since_epoch ) ).count() );
+
+    std::mt19937                 generator( nanoseconds_since_last_second );
     uuids::uuid_random_generator gen( generator );
 
     initField( m_uuid, "UUID" ).withScripting( "UUID", true, false ).withDefault( uuids::to_string( gen() ) );
