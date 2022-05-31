11028f4cb734067d150fad0dca8115b5b17106f8
Gaute Lindkvist <lindkvis@gmail.com>
2022-05-31 08:54:43 +0200

    Rename caffa-logger -> caffa-base and add a not_null implementation from GSL

--- GrpcInterface/GrpcInterface_TestData/DemoObject.h
+++ GrpcInterface/GrpcInterface_TestData/DemoObject.h
@@ -21,10 +21,9 @@
 #include "cafAppEnum.h"
 #include "cafDocument.h"
 #include "cafField.h"
+#include "cafNotNull.h"
 #include "cafObject.h"
 #include "cafObjectMethod.h"
-
-#include <gsl/gsl>
 
 class DemoObject : public caffa::Object
 {
@@ -123,13 +122,13 @@ public:
     {
         CAFFA_DEBUG( "Executing object method on server with values: " << m_doubleField() << ", " << m_intField()
                                                                        << ", " << m_stringField() );
-        gsl::not_null<DemoObject*> demoObject = self<DemoObject>();
-        demoObject->doubleField               = m_doubleField;
-        demoObject->intField                  = m_intField;
-        demoObject->stringField               = m_stringField;
-        demoObject->intVector                 = m_intVector;
-        demoObject->boolVector                = m_boolVector;
-        demoObject->floatVector               = m_floatVector;
+        caffa::not_null<DemoObject*> demoObject = self<DemoObject>();
+        demoObject->doubleField                 = m_doubleField;
+        demoObject->intField                    = m_intField;
+        demoObject->stringField                 = m_stringField;
+        demoObject->intVector                   = m_intVector;
+        demoObject->boolVector                  = m_boolVector;
+        demoObject->floatVector                 = m_floatVector;
 
         auto demoObjectResult    = std::make_unique<DemoObject_copyObjectResult>();
         demoObjectResult->status = true;
