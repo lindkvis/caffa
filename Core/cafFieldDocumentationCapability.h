075ad95efdc5f974444544a4d402582916f2e139
Gaute Lindkvist <lindkvis@gmail.com>
2022-12-28 14:42:42 +0000

    Add a documentation capability for fields

---
+++ Core/cafFieldDocumentationCapability.h
@@ -0,0 +1,39 @@
+//##################################################################################################
+//
+//   Caffa
+//   Copyright (C) 2022- Kontur AS
+//
+//   GNU Lesser General Public License Usage
+//   This library is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation; either version 2.1 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
+//   for more details.
+//
+//##################################################################################################
+#pragma once
+
+#include "cafFieldCapability.h"
+
+#include <string>
+
+namespace caffa
+{
+class FieldDocumentationCapability : public FieldCapability
+{
+public:
+    FieldDocumentationCapability( const std::string& documentation );
+
+    std::string documentation() const;
+
+private:
+    std::string m_documentation;
+};
+
+} // namespace caffa
