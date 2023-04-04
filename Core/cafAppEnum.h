e97b6132e4fb17b3615ef1eff77fee3604bb8538
Gaute Lindkvist <lindkvis@gmail.com>
2023-04-04 12:21:31 +0200

    Require C++20

--- Core/cafAppEnum.h
+++ Core/cafAppEnum.h
@@ -1,24 +1,24 @@
-//##################################################################################################
+// ##################################################################################################
 //
-//   Custom Visualization Core library
-//   Copyright (C) 2011-2013 Ceetron AS
-//   Copyright (C) 2013- Ceetron Solutions AS
-//   Copyright (C) 2022- Kontur AS
+//    Custom Visualization Core library
+//    Copyright (C) 2011-2013 Ceetron AS
+//    Copyright (C) 2013- Ceetron Solutions AS
+//    Copyright (C) 2022- Kontur AS
 //
-//   GNU Lesser General Public License Usage
-//   This library is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU Lesser General Public License as published by
-//   the Free Software Foundation; either version 2.1 of the License, or
-//   (at your option) any later version.
+//    GNU Lesser General Public License Usage
+//    This library is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation; either version 2.1 of the License, or
+//    (at your option) any later version.
 //
-//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
-//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//   FITNESS FOR A PARTICULAR PURPOSE.
+//    This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//    WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//    FITNESS FOR A PARTICULAR PURPOSE.
 //
-//   See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
-//   for more details.
+//    See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
+//    for more details.
 //
-//##################################################################################################
+// ##################################################################################################
 
 #pragma once
 
@@ -365,6 +365,7 @@ std::istream& operator>>( std::istream& str, caffa::AppEnum<T>& appEnum )
 {
     std::string label;
     str >> label;
+
     appEnum.setFromLabel( label );
 
     return str;
@@ -374,7 +375,7 @@ template <typename T>
 std::ostream& operator<<( std::ostream& str, const caffa::AppEnum<T>& appEnum )
 {
     std::string label = appEnum.label();
-    str << label;
+    str << appEnum.label();
     return str;
 }
 
