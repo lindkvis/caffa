2aaea09c6ce33c29064a3dd02ed65e0bc1593c60
Gaute Lindkvist <lindkvis@gmail.com>
2024-06-13 15:58:43 +0200

    Make sure we join keepalive thread on exit

--- RestInterface/cafRestClient.cpp
+++ RestInterface/cafRestClient.cpp
@@ -253,6 +253,14 @@ RestClient::~RestClient()
     {
         CAFFA_CRITICAL( "Failed to destroy session " << e.what() );
     }
+    try
+    {
+        if ( m_keepAliveThread && m_keepAliveThread->joinable() ) m_keepAliveThread->join();
+    }
+    catch ( const std::exception& e )
+    {
+        CAFFA_ERROR( "Failed to shut down keepalive thread gracefully" );
+    }
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -432,7 +440,7 @@ void RestClient::startKeepAliveThread()
                 try
                 {
                     this->sendKeepAlive();
-                    std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
+                    std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
                 }
                 catch ( ... )
                 {
