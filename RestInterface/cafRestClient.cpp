23342810aabbab882012ffdcd190e4edce17e2d2
Gaute Lindkvist <lindkvis@gmail.com>
2024-06-13 16:07:04 +0200

    Get rid of some compile warnings

--- RestInterface/cafRestClient.cpp
+++ RestInterface/cafRestClient.cpp
@@ -89,7 +89,7 @@ RestClient::~RestClient()
     }
     catch ( const std::exception& e )
     {
-        CAFFA_ERROR( "Failed to shut down keepalive thread gracefully" );
+        CAFFA_ERROR( "Failed to shut down keepalive thread gracefully: " << e.what() );
     }
 }
 
