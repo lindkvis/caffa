77eef350173bcf636a7ff159292a4a53e8131304
Gaute Lindkvist <glindkvist@3d-radar.com>
2024-06-28 16:45:45 +0200

    Reduce automatic submodule to init only

--- cmake/git_submodule.cmake
+++ cmake/git_submodule.cmake
@@ -1,22 +1,22 @@
 find_package(Git REQUIRED)
 
 function(add_git_submodule dir build)
-# add a Git submodule directory to CMake, assuming the
-# Git submodule directory is a CMake project.
-#
-# Usage: in CMakeLists.txt
-# 
-# include(git_submodule.cmake)
-# add_git_submodule(mysubmod_dir)
+    # add a Git submodule directory to CMake, assuming the
+    # Git submodule directory is a CMake project.
+    #
+    # Usage: in CMakeLists.txt
+    #
+    # include(git_submodule.cmake)
+    # add_git_submodule(mysubmod_dir)
 
-if(NOT EXISTS ${dir}/CMakeLists.txt)
-  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${dir}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMAND_ERROR_IS_FATAL ANY)
-endif()
+    if (NOT EXISTS ${dir}/CMakeLists.txt)
+        execute_process(COMMAND ${GIT_EXECUTABLE} submodule init -- ${dir}
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                COMMAND_ERROR_IS_FATAL ANY)
+    endif ()
 
-if(build)
-  add_subdirectory(${dir})
-endif()
+    if (build)
+        add_subdirectory(${dir})
+    endif ()
 
 endfunction(add_git_submodule)
