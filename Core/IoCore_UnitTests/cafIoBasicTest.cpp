7aeff6a3404534400990fcc01bc68a4afebbbf89
Gaute Lindkvist <glindkvist@kontur.tech>
2024-09-23 14:12:35 +0200

    Remove StringStreamers test

--- Core/IoCore_UnitTests/cafIoBasicTest.cpp
+++ Core/IoCore_UnitTests/cafIoBasicTest.cpp
@@ -198,15 +198,14 @@ public:
         doubleProxyAccessor->registerGetMethod( std::bind( &SimpleObj::doubleMember, this ) );
         m_proxyDouble.setAccessor( std::move( doubleProxyAccessor ) );
 
-        initField(m_vectorField, "m_vectorField");
+        initField( m_vectorField, "m_vectorField" );
     }
 
-    caffa::Field<double> m_position;
-    caffa::Field<double> m_dir;
-    caffa::Field<int>    m_up;
-    caffa::Field<double> m_proxyDouble;
+    caffa::Field<double>           m_position;
+    caffa::Field<double>           m_dir;
+    caffa::Field<int>              m_up;
+    caffa::Field<double>           m_proxyDouble;
     caffa::Field<std::vector<int>> m_vectorField;
-
 
     void setDoubleMember( const double& d )
     {
@@ -313,7 +312,7 @@ TEST( BaseTest, TestDataType )
         auto dataType = obj->m_childArrayField.dataType();
 
         // We've stored an InheritedDemoObj in the field, but the field is actually of the parent type DemoObject
-        EXPECT_EQ( ( std::string( "object::" ) + std::string(DemoObject::classKeywordStatic()) + "[]" ), dataType );
+        EXPECT_EQ( ( std::string( "object::" ) + std::string( DemoObject::classKeywordStatic() ) + "[]" ), dataType );
     }
 }
 
@@ -330,28 +329,6 @@ TEST( BaseTest, TestRangeValidation )
     ASSERT_THROW( s1->m_up.setValue( 12 ), std::runtime_error );
     ASSERT_THROW( s1->m_up.setValue( -2 ), std::runtime_error );
     ASSERT_EQ( 5, s1->m_up );
-}
-
-TEST(BaseTest, StringStreamers)
-{
-    auto s1 = std::make_shared<SimpleObj>();
-    s1->m_vectorField.setValue({1, 2, 3});
-
-    std::string string;
-    auto ioCapability = s1->m_vectorField.capability<caffa::FieldIoCapability>();
-    ASSERT_TRUE(ioCapability != nullptr);
-    ioCapability->writeToString(string);
-    CAFFA_INFO("String read from vector field: " << string);
-    string += " 4";
-    ioCapability->readFromString(string);
-
-    ASSERT_EQ(4u, s1->m_vectorField.value().size());
-    ASSERT_EQ(4, s1->m_vectorField.value().back());
-
-    std::string newString;
-    ioCapability->writeToString(newString);
-    CAFFA_INFO("String read from vector field: " << newString);
-
 }
 
 std::string ipsum()
